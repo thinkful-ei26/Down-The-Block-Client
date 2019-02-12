3359953c909eb4c467298ed22fb1b6ed0aade998
Nikkie Mashian <nikkiemashian@gmail.com>
2019-02-12 15:23:25 -0800

    improved forum header

--- src/components/home/ForumHeader.js
+++ src/components/home/ForumHeader.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './forum-header.css';
 
 export default class Forum extends React.Component{
   
   whichForum(){
-    return this.props.type === "neighbor-forum" ? <p>NEIGHBORS FORUM</p> : <p>CITY FORUM</p>
+    return (
+      this.props.type === "neighbor-forum" ? 
+      <h1>What's Happening In Your Neighborhood</h1> : 
+      <h1>What's Happening In Your City</h1>
+    );
   }
 
   render(){
     return(
-      <header> 
+      <header className="forum-header"> 
         {this.whichForum()}
       </header>
     );
