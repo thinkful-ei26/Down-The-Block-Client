7f4dca54f8da656e1e15007a08d27d1f4f3f072d
Stephen Anderson <anderssj@usc.edu>
2019-02-12 22:17:48 -0800

    changes color of signup link from white to black

--- src/components/login-registration/login-page.js
+++ src/components/login-registration/login-page.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link, Redirect } from 'react-router-dom'
 
-import LoginForm from './login-form';
+import LoginForm from './login-form'
 
 export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
-    }
+  // If we are logged in redirect straight to the user's dashboard
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />
+  }
 
-    return (
-        <div className="landing-page">
-            <h2 className="landing-h2">Sign in Bellow</h2>
-            <LoginForm />
-            <label className="landing-register">Don't have an account yet?</label>
-            <Link to="/register" style={{color: 'white'}}>Sign Up!</Link>
-        </div>
-    );
+  return (
+    <div className="landing-page">
+      <h2 className="landing-h2">Sign in Bellow</h2>
+      <LoginForm />
+      <label className="landing-register">Don't have an account yet?</label>
+      <Link to="/register" style={{ color: 'black' }}>
+        Sign Up!
+      </Link>
+    </div>
+  )
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
-});
+  loggedIn: state.auth.currentUser !== null
+})
 
-export default connect(mapStateToProps)(LandingPage);
+export default connect(mapStateToProps)(LandingPage)
