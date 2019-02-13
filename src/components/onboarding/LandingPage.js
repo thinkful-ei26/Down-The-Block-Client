64e02c7904141d028fdd3752840aae917dc659b4
taylormerrill <taylormerrill92@gmail.com>
2019-02-13 09:12:24 -0700

    fixing name issue

--- src/components/onboarding/LandingPage.js
+++ src/components/onboarding/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
-import LoginForm from './LoginForm';
+import LoginForm from './LogInForm';
 import Navbar from '../common/Navbar';
 import './onboarding.css';
 
--- src/components/onboarding/SignupPage.js
+++ src/components/onboarding/SignupPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { Redirect} from 'react-router-dom';
-import SignupForm from './SignupForm';
+import SignupForm from './SignUpForm';
 import './onboarding.css'
 
 export function RegistrationPage(props) {
