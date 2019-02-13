dfa97448ff5bb7deab77950a3255be1defaa080b
Nikkie Mashian <nikkiemashian@gmail.com>
2019-02-13 09:46:17 -0800

    get the users name to show on each comment

--- src/components/home/Comment.js
+++ src/components/home/Comment.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Comment(props){
   return(
     <div className="comment">
+      <p>{props.userId.firstName}</p>
       <p>{props.date}</p>
       <p>{props.content}</p>
     </div>
