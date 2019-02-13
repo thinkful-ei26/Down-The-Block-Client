b6625e1e0f404554552aef25dff6cbe8cbf18124
Nikkie Mashian <nikkiemashian@gmail.com>
2019-02-13 08:07:24 -0800

    fixed a quick typo that was breaking the code

--- src/components/onboarding/SignupPage.js
+++ src/components/onboarding/SignupPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { Redirect} from 'react-router-dom';
 import SignupForm from './SignupForm';
-import './onboarding.css'
+import './onboarding.css';
 
 export function RegistrationPage(props) {
     
