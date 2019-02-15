fa7ea2cfd1f937cdd6b19b780aa08194e82dc61c
taylormerrill <taylormerrill92@gmail.com>
2019-02-15 10:09:33 -0700

    comments working ish, need to show them in real time

--- src/components/home/PostAddComment.js
+++ src/components/home/PostAddComment.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'; 
 import {Field, reduxForm, focus} from 'redux-form';
+import { connect } from 'react-redux';
+import {required, nonEmpty} from '../common/validators';
+import { addComment } from '../../actions/comments';
 
 export class PostAddComment extends Component {
-    onSubmit(values) {
-        return this.props.dispatch(addComment(values.userComment));
+    constructor(props) {
+        super(props);
+    }
+
+    onSubmit(values, props) {
+        props = this.props;
+        return this.props.dispatch(addComment(values.content, props.loggedInUserId, props.form));
     }    
     
     render() {   
@@ -14,15 +22,41 @@ export class PostAddComment extends Component {
                     {this.props.error}
                 </div>
             );
-        }       
-        
+        }   
         return (
-            <div></div>
+                <form 
+                className= "commentInput"
+                onSubmit= {this.props.handleSubmit(values=> 
+                    {
+                    this.onSubmit(values, this.props.loggedInUserId, this.props.form);
+                    this.props.reset('commentInput');
+                    }
+                )}>
+                    {error}
+                    <label htmlFor="comment">ADD COMMENT</label>
+                    <Field
+                        component="input"
+                        type="text"
+                        name="content"
+                        id=  {this.props.postId}
+                        validate={[required, nonEmpty]}
+                    />
+                    <button  disabled={ this.props.pristine || this.props.submitting}>
+                        SUBMIT
+                    </button>
+                </form> 
         )
     }
 }
 
-export default reduxForm({
-    form: 'commentInput',
-    onSubmitFail: (errors, dispatch) => dispatch(focus('userComment'))
-})(AnswerInput);
+PostAddComment = reduxForm({
+})(PostAddComment);
+
+PostAddComment = connect(state => {
+    return{
+        loggedInUserId: state.auth.currentUser.id,
+        postsArray: state.posts.posts  
+    }
+})(PostAddComment)
+
+export default PostAddComment;
