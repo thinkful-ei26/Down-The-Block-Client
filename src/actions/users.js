48d5fa5a4f87dced906e35c129513f16581f71ed
Nikkie Mashian <nikkiemashian@gmail.com>
2019-02-15 16:41:10 -0800

    user can upload profile photo upon registering, and it will be displayed with their post

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,12 +3,30 @@ import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './utils';
 
 export const registerUser = user => dispatch => {
+    let formData = new FormData();
+    
+    Object.keys(user).forEach(item=> {
+        if(item==="img" && user[item].public_id){
+            formData.append('public_id', user[item].public_id)
+            formData.append('url', user[item].url)
+        }
+        else{
+            formData.append(item, (user[item]))
+        }
+    });
+
+    for (let pair of formData.entries()) {
+        console.log('DATA', pair[0]+ ', ' + pair[1]); 
+    }
+
+    console.log(formData);
+
     return fetch(`${API_BASE_URL}/auth/users`, {
         method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(user)
+        // headers: {
+        //     'content-type': 'application/json'
+        // },
+        body: formData
     })
         .then(res => normalizeResponseErrors(res))
         .then(res =>{ 
