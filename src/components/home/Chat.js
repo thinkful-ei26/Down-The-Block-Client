3087aa08ee0295644ca8c6171a6d861a20de1ab7
Stephen Anderson <anderssj@usc.edu>
2019-02-19 08:28:19 -0800

    chat style

--- src/components/home/Chat.js
+++ src/components/home/Chat.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import socketClient from "socket.io-client"
+import './Chat.css'
 
 export class Chat extends React.Component{
   constructor(props){
