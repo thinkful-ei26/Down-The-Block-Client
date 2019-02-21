a529a32d9313ca50ec31dcf3719bb6270834da40
Stephen Anderson <anderssj@usc.edu>
2019-02-21 09:24:31 -0800

    progress on users in the sidebar to enter chats with

--- src/components/home/Chat.js
+++ src/components/home/Chat.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import socketClient from "socket.io-client"
+import {API_BASE_URL} from '../../config'
 import './Chat.css'
 
-export class Chat extends React.Component{
-  constructor(props){
+export class Chat extends React.Component {
+  constructor(props) {
     super(props)
-    this.state={
+    this.state = {
       messages: []
     }
-    this.socket = socketClient("http://localhost:8080")
-    this.socket.on("chat-message", (msg)=>{
-      this.setState({messages: [...this.state.messages, msg]})})
+    this.socket = socketClient(API_BASE_URL)
+    this.socket.on("chat-message", (msg) => {
+      this.setState({ messages: [...this.state.messages, msg] })
+    })
   }
-  render(){
-    return(
+  render() {
+    return (
       <section className="chat">
-        <h1>Chat Messages</h1>
-        <form onSubmit={e => {
-          e.preventDefault()
-          this.socket.emit("chat-message", e.currentTarget.message.value )
-          console.log("submitted")
-        }} >
-          <input type="text" placeholder="Type your message" name="message"
-          />
-          <button type="submit">Submit</button>
-        </form>
-        {this.state.messages.map(message=>{
-          return <li>{message}</li>
-        })}
+        <div className="chat"> <h1>Chat Messages</h1>
+          <form onSubmit={e => {
+            e.preventDefault()
+            this.socket.emit("chat-message", e.currentTarget.message.value)
+            console.log("submitted")
+          }} >
+            <input className="type_msg" type="text" placeholder="Type your message" name="message"
+            />
+            <button type="submit">Submit</button>
+          </form>
+          {this.state.messages.map(message => {
+            return <li className="msg_container">{message}</li>
+          })}</div>
       </section>
     );
   }
--- src/components/home/Sidebar.js
+++ src/components/home/Sidebar.js
@@ -3,9 +3,36 @@ import {connect} from 'react-redux';
 import {display} from '../../actions/navigation'
 import './sidebar.css'
 import { fetchPosts } from '../../actions/posts';
+import REACT_APP_API_BASE_URL from '../../config'
 
 class Sidebar extends React.Component{
-  render(){
+  constructor(props){
+    super(props)
+    this.state={
+      users: {},
+
+    }
+  }
+
+  componentDidMount = async () => {
+    const response = await fetch(`http://localhost:8080/auth/users/`)
+    const json = await response.json()
+    this.setState({ users: json });
+
+  }
+
+  getListOfUsers= ()=>{
+    let arrayofuserobjects = []
+      for(let name in this.state.users){
+        arrayofuserobjects.push(this.state.users[name])
+        console.log(this.state.users[name].id, this.capitalizeFirstLetter(this.state.users[name].username))
+      }
+      return arrayofuserobjects
+  }
+ capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+ render(){
     return(
       <aside className="sidebar">
         <nav>
@@ -14,6 +41,11 @@ class Sidebar extends React.Component{
           <button onClick={()=>this.props.dispatch(fetchPosts(this.props.coords, 'city'))} >City</button>
           <h4>Chats</h4>
           <button  onClick={()=>this.props.dispatch(display('chat'))}>Users</button>
+          <button onClick={()=>{this.getListOfUsers()}}>Get users</button>
+          <ul>
+            <li>Nikki</li>
+            <li>Steve</li>
+          </ul>
         </nav>
       </aside>
     );
@@ -21,7 +53,7 @@ class Sidebar extends React.Component{
 }
 const mapStateToProps = state => ({
   coords: state.geolocation.coords,
-  display: state.nav.display 
+  display: state.nav.display
 });
 
 export default connect(mapStateToProps)(Sidebar)
