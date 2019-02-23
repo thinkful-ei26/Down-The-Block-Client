8b06aa19a4ca1eaa3c0fdd2ba426cfddfade8525
Nikkie Mashian <nikkiemashian@gmail.com>
2019-02-22 17:05:19 -0800

    code cleanup

--- src/components/home/CreatePost.js
+++ src/components/home/CreatePost.js
@@ -27,8 +27,6 @@ export class CreatePost extends React.Component{
   }
 
   componentDidMount(){
-    // this.props.socket.connect();
-    console.log('IN COMPONENT DID MOUNT FOR CREATE POST')
     //if editing, highlight the correct chosen category
     if(this.props.editPost){
       this.setState({
@@ -51,10 +49,9 @@ export class CreatePost extends React.Component{
   }
 
   componentWillUnmount(){
-    console.log('UNMOUNTING CREATE POST')
+    //need to turn sockets off or else they'll listen twice
     this.props.socket.off('new_post');
     this.props.socket.off('edited_post');
-    // this.props.socket.disconnect();
   }
 
   generateButtons(){
--- src/reducers/socket.js
+++ src/reducers/socket.js
@@ -1,6 +1,5 @@
 import socketClient from "socket.io-client";
 import { API_BASE_URL } from '../config'; 
-import { DESTROY_SOCKET, CREATE_SOCKET } from '../actions/types'
 
 export const socket = socketClient(`${API_BASE_URL}`);
 
@@ -11,19 +10,6 @@ const initialState={
 
 export default (state=initialState, action) => {
     switch (action.type) {	
-        case DESTROY_SOCKET:
-            return Object.assign({}, state, {
-                socket: null,
-            })
-        case CREATE_SOCKET:
-            if(state.socket===null){
-                return Object.assign({}, state, {
-                    socket: socketClient(`${API_BASE_URL}`)
-                })
-            }
-            else{
-                return state;
-            }
         default: 
             return state
     }
