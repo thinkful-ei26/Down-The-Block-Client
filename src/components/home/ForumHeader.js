bdb279d80afa1308e65a9b09ec81d6ff7853806b
Ethan Frigon <ethan.frigon@gmail.com>
2019-02-26 12:14:10 -0800

    select dropdown now integrates colored labels

--- src/components/home/ForumHeader.js
+++ src/components/home/ForumHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Select from 'react-select';
+import chroma from 'chroma-js';
 import { changeSearchTerm, changeCategoryFilter } from '../../actions/posts'
 import './forum-header.scss';
 
@@ -16,19 +17,64 @@ export class ForumHeader extends React.Component{
 
   render(){
     const filterOptions = [
-      {value:"", label:"Filter Category:"},
-      {value: "Crime", label: "Crime"},
-      {value: "Personal", label: "Personal"},
-      {value: "Event", label: "Event"},
-      {value: "Other", label: "Other"}
+      {value: "", label:"See Category:", color: "#ccc"},
+      {value: "Crime", label: "Crime", color: '#e36060'},
+      {value: "Personal", label: "Personal", color: '#45bb85'},
+      {value: "Event", label: "Event", color: '#c093d7'},
+      {value: "Other", label: "Other", color: '#f3c59c'}
     ];
+
+    const dot = (color = '#ccc') => ({
+      alignItems: 'center',
+      display: 'flex',
+    
+      ':before': {
+        backgroundColor: color,
+        borderRadius: 10,
+        content: '" "',
+        display: 'block',
+        marginRight: 8,
+        height: 10,
+        width: 10,
+      },
+    });
+
+    const colorStyles = {
+      control: styles => ({ ...styles, backgroundColor: 'white' }),
+      option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+        console.log('here');
+        const color = chroma(data.color);
+        return {
+          ...styles,
+          backgroundColor: isDisabled
+            ? null
+            : isSelected ? data.color : isFocused ? color.alpha(0.1).css() : null,
+          color: isDisabled
+            ? '#ccc'
+            : isSelected
+              ? chroma.contrast(color, 'white') > 2 ? 'white' : 'black'
+              : data.color,
+          cursor: isDisabled ? 'not-allowed' : 'default',
+        };
+      },
+      input: styles => ({ ...styles, ...dot() }),
+      placeholder: styles => ({ ...styles, ...dot() }),
+      singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) }),
+    };
 
     return(
       <header className="forum-header">
         <input type="text" onChange={e=>this.props.dispatch(changeSearchTerm(e.target.value))} className="search-posts" placeholder="Search Forum"/> 
 
         {this.whichForum()}
-        <Select options={filterOptions} placeholder="Filter Category" className="filter-posts" onChange={option => this.props.dispatch(changeCategoryFilter(option.value))} />
+        <Select
+          defaultValue={filterOptions[0]} 
+          options={filterOptions} 
+          placeholder="See Category" 
+          className="filter-posts" 
+          onChange={option => this.props.dispatch(changeCategoryFilter(option.value))}
+          styles={colorStyles}
+        />
 
       </header>
     );
