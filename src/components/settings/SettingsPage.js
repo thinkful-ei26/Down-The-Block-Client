48010ebabab976c7d78817e47f2b385db4f67285
Stephen Anderson <anderssj@usc.edu>
2019-02-25 16:28:14 -0800

    styling on forms

--- src/components/settings/SettingsPage.js
+++ src/components/settings/SettingsPage.js
@@ -4,7 +4,7 @@ import UpdateAccountForm from './UpdateAccountForm';
 import UpdatePasswordForm from './UpdatePasswordForm';
 import requiresLogin from '../common/requires-login';
 import {changeSuccessMessage, updateProfilePhoto} from '../../actions/users';
-import './settings.css'
+// import './settings.css'
 
 export class SettingsPage extends React.Component{
   componentDidMount(){
@@ -14,7 +14,7 @@ export class SettingsPage extends React.Component{
   componentDidUpdate(){
     window.scrollTo(0, 0); //make it jump to top
     if(this.props.successMessage){
-    
+
       //set a timer that changes the message back to null after seven seconds of displaying it
     setTimeout(
         function() {
@@ -39,25 +39,25 @@ export class SettingsPage extends React.Component{
     return(
       <div className="settings">
         <main className="settings-main">
-          {this.props.successMessage && 
+          {this.props.successMessage &&
           <div className="updated-message" aria-live="polite">
             {this.props.successMessage}
           </div>
           }
           <div className="profile-photo-avatar">
-            {!this.props.currentUser.photo ? 
+            {!this.props.currentUser.photo ?
               <p className="initials">
                 {this.props.currentUser.firstName[0]}
                 {this.props.currentUser.lastName[0]}
               </p>
               :
-              <img className="profile-photo" src={this.props.currentUser.photo.url} alt="profile"/> 
+              <img className="profile-photo" src={this.props.currentUser.photo.url} alt="profile"/>
             }
           </div>
           <label className="image-input">
             <span>Update Your Profile Photo</span>
-            <input 
-              onChange={(e)=>this.onSubmit(e)} 
+            <input
+              onChange={(e)=>this.onSubmit(e)}
               accept="image/*"
               ref={input => this.img = input}
               type="file"
