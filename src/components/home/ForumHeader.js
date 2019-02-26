c46ad9f15a6b663e83d4fe92c97e502bda2dda63
Ethan Frigon <ethan.frigon@gmail.com>
2019-02-26 11:08:38 -0800

    react-select module added for styling

--- src/components/home/ForumHeader.js
+++ src/components/home/ForumHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Select from 'react-select';
 import { changeSearchTerm, changeCategoryFilter } from '../../actions/posts'
 import './forum-header.scss';
 
@@ -14,19 +15,31 @@ export class ForumHeader extends React.Component{
   }
 
   render(){
+    const filterOptions = [
+      {value:"", label:"Filter Category:"},
+      {value: "Crime", label: "Crime"},
+      {value: "Personal", label: "Personal"},
+      {value: "Event", label: "Event"},
+      {value: "Other", label: "Other"}
+    ];
+
+    // const selectComponent = () => (
+    //   <Select options={filterOptions} />
+    // );
     return(
       <header className="forum-header">
         <input type="text" onChange={e=>this.props.dispatch(changeSearchTerm(e.target.value))} className="search-posts" placeholder="Search Forum"/> 
 
         {this.whichForum()}
+        <Select options={filterOptions} placeholder="Filter Category" className="filter-posts" onChange={option=>this.props.dispatch(changeCategoryFilter(option.value))} />
 
-        <select className="filter-posts" onChange={e=>this.props.dispatch(changeCategoryFilter(e.target.value))}>
+        {/* <select className="filter-posts" onChange={e=>this.props.dispatch(changeCategoryFilter(e.target.value))}>
           <option value="">Filter Category:</option>
           <option value="Crime">Crime</option>
           <option value="Personal">Personal</option>
           <option value="Event">Event</option>
           <option value="Other">Other</option>
-        </select>
+        </select> */}
       </header>
     );
   }
