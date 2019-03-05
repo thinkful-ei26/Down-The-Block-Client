01156cfd05be8b55b3b0660f737fdf6ebf938308
Nikkie Mashian <nikkiemashian@gmail.com>
2019-03-04 18:40:54 -0800

    improved address form

--- src/components/home/AddressForm.js
+++ src/components/home/AddressForm.js
@@ -6,7 +6,6 @@ import { showAnimation } from '../../actions/navigation';
 import { fetchAddressSuccess } from '../../actions/geolocation';
 import { setUserCoords } from '../../actions/users';
 
- 
 class AddressForm extends React.Component {
   constructor(props) {
     super(props);
@@ -43,47 +42,50 @@ class AddressForm extends React.Component {
  
   render() {
     return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-          <div className="addressDiv">
-            <section className="parallax"></section>
-            <section className="intro">
-              <section className="form-section address-form">
-                <input
-                  {...getInputProps({
-                    placeholder: 'Enter your address here...',
-                    className: 'location-search-input',
-                  })}
-                />
-                <div className="autocomplete-dropdown-container">
-                  {suggestions.map(suggestion => {
-                    const className = suggestion.active
-                      ? 'suggestion-item--active'
-                      : 'suggestion-item';
-                      const style = suggestion.active
-                      ? { backgroundColor: '#06311f', color: 'rgb(237, 236, 217)', cursor: 'pointer' }
-                      : { backgroundColor: 'rgb(237, 236, 217)', cursor: 'pointer' };
-                    return (
-                      <div
-                        {...getSuggestionItemProps(suggestion, {
-                          className,
-                          style
-                        })}
-                      >
-                        <span>{suggestion.description}</span>
-                      </div>
-                    );
-                  })}
-                </div>
-                </section>
-            </section>
-          </div>
-        )}
-      </PlacesAutocomplete>
+      <div className="addressDiv">
+        <section className="parallax"></section>
+        <section className="intro">
+          <section className="address-container">
+          <h1>Find Your Neighborhood</h1>
+          <p className="text">Your browser hasn't allowed DownTheBlock permission to your location. However, to be placed in the correct neighborhood, DownTheBlock needs access to your location. Please either change the settings in your browser and refresh, or manually input your address below.</p>
+          <PlacesAutocomplete
+            value={this.state.address}
+            onChange={this.handleChange}
+            onSelect={this.handleSelect}
+          >
+            {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+              
+                  <section className="address-form">
+                    <input
+                      {...getInputProps({
+                        placeholder: 'Enter your address here...',
+                        className: 'location-search-input',
+                      })}
+                    />
+                    <div className="autocomplete-dropdown-container">
+                      {suggestions.map(suggestion => {
+                        const className = 'suggestion-item';
+                        //   const style = suggestion.active
+                        //   ? { backgroundColor: '#06311f', color: 'rgb(237, 236, 217)', cursor: 'pointer' }
+                        //   : { backgroundColor: 'white', cursor: 'pointer' };
+                        return (
+                          <div
+                            {...getSuggestionItemProps(suggestion, {
+                              className,
+                              // style
+                            })}
+                          >
+                            <span>{suggestion.description}</span>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    </section>
+            )}
+          </PlacesAutocomplete>
+        </section>
+        </section>
+      </div>
     );
   }
 }
